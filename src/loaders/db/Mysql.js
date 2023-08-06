974102a192f0292c1d698ed8d31162362cbc6204
sejin <tpwls8122@naver.com>
2023-08-07 01:22:18 +0900

    mysql db config

---
+++ src/loaders/db/Mysql.js
@@ -0,0 +1,20 @@
+import config from '../../config/index.js';
+import mysql2 from "mysql2/promise";
+
+const pool = mysql2.createPool(config);
+
+export default async (query) => {
+    try {
+        const conn = await pool.getConnection(async con => con)
+        try {
+            const [rows] = await conn.query(query);
+            return rows;
+        } catch(err) {
+            return false;
+        } finally {
+            conn.release();
+        }
+    } catch(err) {
+        return false;
+    }
+}
