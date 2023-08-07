281eaf1e94da68f2e11936b32373eae7ed5fcc84
sejin <tpwls8122@naver.com>
2023-08-07 15:36:10 +0900

    add api middleware

--- src/loaders/routers/index.js
+++ src/loaders/routers/index.js
@@ -1,12 +1,22 @@
 import User from "./User.js";
 import Keyword from "./Keyword.js";
+import logger from "../app/Wiston.js"
 
 export default (app) => {
+  
+  app.use((req, res, next) => {
+    if (process.env.NODE_ENV !== "production") {
+      logger.info(`[${req.method}][${req.url}]`)
+    }
+    next()
+  })
+
   app.head("/status", (req, res) => {
     res.status(200).end();
   });
 
   app.use("/user", User);
   app.use("/keyword", Keyword);
+  
   return app;
 };
