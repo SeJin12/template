bfbef0aac8c6da1a5ece844ca54c64e7d6a23169
sejin <tpwls8122@naver.com>
2023-08-07 10:14:42 +0900

    winston + morgan

--- src/loaders/app/Express.js
+++ src/loaders/app/Express.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import Router from "../routers/index.js";
+import morganMiddleware from "./Morgan.js";
 
 const Express = (app) => {
   app.use(bodyParser.json()); // request body => json
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cors({ credentials: true, origin: "*" }));
   app.use(express.json({ limit: "16mb" }));
+
+  app.use(morganMiddleware);
 
   app.head("/status", (req, res) => {
     res.status(200).end();
